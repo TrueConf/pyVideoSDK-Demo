78c4704b857ddbf9bfd6d294f2f5d2abd4a8faca
Andrey Zobov <zoboff@gmail.com>
2022-05-31 16:13:06 +0300

    fixed 2 samples

--- videocall.py
+++ videocall.py
@@ -15,7 +15,10 @@ methods = Methods(sdk)
 @sdk.handler(EVENT[C.EV_appStateChanged])
 @sdk.handler(METHOD_RESPONSE[C.M_getAppState])
 def on_state_change(response):
-    print(f'    @@@ {response["appState"]}')
+    print(f'    Application state is {response["appState"]}')
+
+    if response["appState"] == 5:
+        print("\nDone! We are in the conference!\n")
 
 @sdk.handler(EVENT[C.EV_rejectReceived])
 def on_reject(response):
@@ -30,5 +33,6 @@ def on_all(response):
 '''
 
 if __name__ == '__main__':
-    methods.call("echotest@trueconf.com")
+    print("Calling...")
+    methods.call("\c\demo_conf")
     sdk.run()
